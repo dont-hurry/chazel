b82618031d22e309baf7470aa6039e30c8e2a18d
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-08-31 10:54:01 +0800

    Add header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,12 @@
+import styles from "./Header.module.css";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={styles.container}>
+      <h1 className={styles.heading}>Chazel</h1>
+      <h2 className={styles.description}>
+        「我是塞車之神，你也是塞車之神，這座島上的所有人，都是塞車之神。」
+      </h2>
+    </header>
+  );
 }
