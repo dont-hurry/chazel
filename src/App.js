eb3bbdc1027e75b30eb31487b8121a0b80b9eab8
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-08-31 10:41:22 +0800

    Add boilerplate components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,16 @@
+import styles from "./App.module.css";
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div className={styles.container}>
+      <Header />
+      <Navigation />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
