e57db55e59116f1c513c40b1109abedd373d6189
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-09-03 13:52:23 +0800

    Update footer

--- src/components/Footer/Column.js
+++ src/components/Footer/Column.js
@@ -1,10 +1,14 @@
 import styles from "./Column.module.css";
 
-export default function Column({ imageSource, text }) {
+export default function Column({ imageSource, name, content }) {
   return (
     <div className={styles.container}>
       <img src={imageSource} alt="" className={styles.avatar} />
-      <div className={styles.text}>{text}</div>
+
+      <div className={styles.textContainer}>
+        <div className={styles.textName}>{name}</div>
+        {content && <div className={styles.textContent}>{content}</div>}
+      </div>
     </div>
   );
 }
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,8 +6,12 @@ export default function Footer() {
     <footer className={styles.container}>
       <div className={styles.gridWrapper}>
         <div className={styles.about}>關於</div>
-        <Column imageSource="/images/chazel.jpeg" text="文章 - 陳建佐 Chazel" />
-        <Column imageSource="/images/pohan.jpeg" text="網站 - 陳柏翰" />
+        <Column
+          imageSource="/images/chazel.jpeg"
+          name="文章 - 陳建佐 Chazel"
+          content="想要寫關於鯨魚的故事"
+        />
+        <Column imageSource="/images/pohan.jpeg" name="網站 - 陳柏翰" />
       </div>
     </footer>
   );
