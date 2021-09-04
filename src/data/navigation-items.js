92ab9e7d1e5ececc161424e1e25165d44c196b8f
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-09-04 21:52:02 +0800

    Fix navigation link reference

--- src/data/navigation-items.js
+++ src/data/navigation-items.js
@@ -1,8 +1,8 @@
 const navigationItems = [
-  { href: "#novel", text: "長篇小說 - 泣城" },
-  { href: "#short-story", text: "短篇小說" },
-  { href: "#flash-fiction", text: "極短篇小說" },
-  { href: "#seven-library", text: "七號圖書館" },
+  { href: "/#novel", text: "長篇小說 - 泣城" },
+  { href: "/#short-story", text: "短篇小說" },
+  { href: "/#flash-fiction", text: "極短篇小說" },
+  { href: "/#seven-library", text: "七號圖書館" },
   {
     href: "http://home.gamer.com.tw/homeindex.php?owner=Chazel315",
     target: "_blank",
