4e0afa041fccec4ded2950995e6a24039a895fc6
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-09-06 00:15:11 +0800

    Add react-loading-skeleton

--- src/pages/reading.js
+++ src/pages/reading.js
@@ -2,34 +2,30 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getArticleByPathAndArticleId } from "../services/articles";
 import styles from "./reading.module.css";
+import Skeleton from "react-loading-skeleton";
 
-// TODO: Move these options into sidebar component
-const fontSizeOptions = [14, 18, 22];
-const lineHeightOptions = [1.6, 2, 2.4];
-
-export default function Reading() {
+export default function Reading({ fontSize, lineHeight, setLineHeight }) {
   const [article, setArticle] = useState(null);
-  const [fontSize, setFontSize] = useState(18);
-  const [lineHeight, setLineHeight] = useState(2);
 
   const { path, articleId } = useParams();
 
   useEffect(() => {
-    getArticleByPathAndArticleId(path, articleId).then((data) =>
-      setArticle(data)
+    getArticleByPathAndArticleId(path, articleId).then((returnedArticle) =>
+      setArticle(returnedArticle)
     );
   }, [path, articleId]);
 
-  if (!article) return null;
-
   return (
     <div className={styles.outerContainer}>
-      <h2 className={styles.title}>{article.title}</h2>
+      {article && <h2 className={styles.title}>{article.title}</h2>}
+      {!article && <Skeleton className={styles.skeletonTitle} />}
 
       <div className={styles.innerContainer}>
         <div className={styles.contentWrapper} style={{ fontSize, lineHeight }}>
-          {article.content}
+          {article && article.content}
+          {!article && <Skeleton count="10" />}
         </div>
+
         <div className={styles.sidebar}>sidebar</div>
       </div>
     </div>
