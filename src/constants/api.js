1d072d73681b08de075794073b3f8e388701ed98
Pohan Chen <89617594+dont-hurry@users.noreply.github.com>
2021-11-29 03:26:34 +0800

    Change backend address

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,2 +1,2 @@
-// export const baseUrl = "https://stormy-headland-37546.herokuapp.com/api";
-export const baseUrl = "http://localhost:3001/api";
+export const baseUrl = "https://stormy-headland-37546.herokuapp.com/api";
+// export const baseUrl = "http://localhost:3001/api";
